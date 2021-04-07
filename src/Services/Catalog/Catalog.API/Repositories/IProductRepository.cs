847edbfa653775f0b0aed0563609df5824b0ea93
Neven Dimač <neven.dimac@gmail.com>
2021-04-07 10:30:10 +0200

    Added catalog repository, product controller and registered in startup class

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<Product> GetProductByName(string name);
+        Task<Product> GetProductByCategory(string category);
+
+
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+
+
+    }
+}
