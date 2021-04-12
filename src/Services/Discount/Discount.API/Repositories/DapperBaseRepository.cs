c319c7514dbb1614e4b91fd9d99fda5c4ac9a029
Neven Dimač <neven.dimac@gmail.com>
2021-04-12 17:53:35 +0200

    added baserepository for dapper, added interfaces and repositories

---
+++ src/Services/Discount/Discount.API/Repositories/DapperBaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DapperBaseRepository
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public DapperBaseRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<T> QueryFirstOrDefaultAsync<T>(string query, object parameters = null)
+        {
+            try
+            {
+                using NpgsqlConnection conn
+                       = new(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                return  await conn.QueryFirstOrDefaultAsync<T>(query, parameters);
+            }
+            catch (Exception)
+            {
+                //Handle the exception
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : DapperBaseRepository, IDiscountRepository
+    {
+        public DiscountRepository(IConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            var coupon = await QueryFirstOrDefaultAsync<Coupon>("SELECT * FROM Coupon WHERE ProductName = @ProductName",  new { ProductName = productName });
+            if (coupon == null)
+            {
+                return new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+            }
+            return coupon;
+        }
+
+
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
