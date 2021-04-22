3dcf8e21f2c7b012b3f24add2496d00564a560c0
Neven Dimač <neven.dimac@gmail.com>
2021-04-22 09:52:36 +0200

    added host extensions for seeding orderdb aded image for sql

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ordering.API.Extensions;
+using Ordering.Infrastructure.Persistance;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.API
 {
@@ -13,7 +11,14 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+          CreateHostBuilder(args)
+               .Build()
+               .MigrateDatabase<OrderContext>((context, services) =>
+                {
+                    var logger = services.GetService<ILogger<OrderContexSeed>>();
+                    OrderContexSeed.SeedAsync(context,logger).Wait();
+                })
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
