dd99a50005b529aaa613a5edc615a3e6e976ff22
Neven Dimač <neven.dimac@gmail.com>
2021-04-27 11:42:51 +0200

    rebuilded docker files, added docker compose.overriide rabbitmq properties

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediatr;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediatr, ILogger<BasketCheckoutConsumer> logger)
+        {
+            _mapper = mapper;
+            _mediatr = mediatr;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediatr.Send(command);
+            _logger.LogInformation("Basketcheckout consumded successfully. Created order Id: {newOrderId}", result);
+        }
+    }
+}
